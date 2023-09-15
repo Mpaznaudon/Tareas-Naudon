f942caea11d42804d0e795035e05ea515130dd47
Mpaznaudon <mpaznaudon@uc.cl>
2023-09-15 12:30:58 -0300

    entrega tarea 2 con comentarios

--- Tareas/tarea 2/tarea_2_naudon_m_paz.py
+++ Tareas/tarea 2/tarea_2_naudon_m_paz.py
@@ -28,6 +28,8 @@ else:
 
 """1. Hacer un programa que imprima los números del 1 al 100, en python sería:"""
 
+#comentario: importante print por o si no, no transcribe los codigos
+
 print (range(100))
 
 for x in range(100):
@@ -35,7 +37,6 @@ for x in range(100):
 
 """
 2. **Hacer un programa que imprima los números del 1 al 100 que sean divisibles entre 3 (con resto 0)**
-
 Esto signifca que si el número es 3, 6, 9, 12, 15, etc. entonces debe imprimirlo ya que la división de esos números por 3 da como resto 0, exacto.
 
 3 / 3 = 1, resto 0
@@ -45,6 +46,8 @@ Distinto de:
 4 / 3 = 1, resto 1
 Hint de ayuda: en programación, hay un símbolo que ocupamos para saber inmediatamente el resto, en lugar de hacer la división y luego ver el resto. En python es el símbolo %, por ejemplo:"""
 
+#comentario: for: permite realizar operaciones repetitivas de manera sencilla y eficiente.
+
 for x in range(100):
   if (x+1) % 3 == 0:
     print(x+1)
@@ -53,6 +56,8 @@ for x in range(100):
 
  Realiza el algoritmo para un sumador de dos números. Si el resultado es menor a 100 se mostrará el mensaje "menor a 100", si el resultado es mayor a 100, pero menor a 150 mostrará el mensaje "mayor a 100", pero si es mayor a 150 mostrará el mensaje "mayor a 150".
 """
+
+#comentario: if: permite ejecutar el código Python si se cumple una condición. elif: se usa para comprobar múltiples condiciones si una condición es falsa
 
 numero_1 = int(input("Ingresa un número"))
 numero_2 = 5
@@ -71,6 +76,8 @@ print(resultado)
 """**Problema 4. Pseudocódigo, Calculador de promedio**
 Generar un programa que combine variables, tipos de datos y una condicional en Python (hacer una función que evalúe si el usuario es mayor de edad y si le gusta la programación, que envíe diferentes textos según la respuesta del usuario)
 """
+
+#comentario: def: indica a Python que una nueva función está siendo definida. lower: baja todo a minúscula
 
 def calculador_de_promedio():
  nombre = input("ingresa nombre: ")
